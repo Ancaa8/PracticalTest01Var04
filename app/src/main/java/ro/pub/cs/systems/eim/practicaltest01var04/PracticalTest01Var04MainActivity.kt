1d0fd1c5b931faa07d6100d8ef2bff1205db7375
Ancaa8 <anca_teodora.andrei@stud.acs.upb.ro>
2025-11-10 18:27:23 +0200

    Commit a

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
@@ -0,0 +1,21 @@
+package ro.pub.cs.systems.eim.practicaltest01var04
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PracticalTest01Var04MainActivity : AppCompatActivity() {
+
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_practical_test01_var04_main)
+
+    }
+}
